7abada69af54d29476e1284875ba555c288964b9
Dasha <dasha.peregudova@gmail.com>
2016-11-30 13:37:31 +0200

    Update arr2.js

--- arr2.js
+++ arr2.js
@@ -4,3 +4,15 @@
 Функция должна вернуть массив с добавленным
 новым элементом.
 */
+var arr = [1, 2, 3, 4, 5, "last"];
+
+function lastItem(arr, item){
+    arr.push(item)
+   console.log(arr);
+}
+
+lastItem(arr, "new last");
+lastItem(arr, "new last");
+lastItem(arr, "new last");
+lastItem(arr, "new last");
+
