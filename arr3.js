f6edd475fb72250f1b573e5c4a9d20a85a042cd8
Dasha <dasha.peregudova@gmail.com>
2016-11-30 13:45:53 +0200

    Update arr3.js

--- arr3.js
+++ arr3.js
@@ -7,3 +7,26 @@
 4. Удалите первое значение массива и выведите его console.
 5. Добавьте в начало значения «apricot» и «peach».
 */
+var fruit = ["apple", "orange"]; //Создайте массив fruits с элементами «apple», «orange».
+
+fruit.push("kiwi"); //Добавьте в конец значение «kiwi»
+
+console.log(fruit);
+
+fruit[fruit.length - 2] = "pear"; //Замените предпоследнее значение с конца на «pear»
+
+console.log(fruit);
+
+console.log(fruit.shift()); //Удалите первое значение массива и выведите его console.
+
+console.log(fruit);
+
+fruit.unshift("apricot", "peach"); // Добавьте в начало значения «apricot» и «peach».
+
+console.log(fruit);
+
+
+
+
+
+
