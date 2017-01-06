65c3ea3df3adb57b6455a0114789615d75232ab0
Dasha <dasha.peregudova@gmail.com>
2017-01-06 15:23:10 +0200

    Create 6.2_arrSorted(in_proccess).js

---
+++ 6.2_arrSorted(in_proccess).js
@@ -0,0 +1,18 @@
+/* Есть массив строк arr. Создайте массив arrSorted —
+из тех же элементов, но отсортированный.
+Исходный массив не должен меняться.
+*/
+
+var arr = ["HTML", "JavaScript", "CSS"];
+
+function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+}
+
+var newArr = arr.sort(compareNumeric);
+
+
+console.log( newArr ); // CSS, HTML, JavaScript
+console.log( arr ); // HTML, JavaScript, CSS (без изменений)
